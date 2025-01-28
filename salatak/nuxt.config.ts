6a3855e3adba2629bbb565efc4c91511c7958fad
Mohamed Elhefni <elhefnymohamed446@gmail.com>
2025-01-28 16:10:57 +0200

    chore: use tailgraph for og images

--- salatak/nuxt.config.ts
+++ salatak/nuxt.config.ts
@@ -31,16 +31,16 @@ export default defineNuxtConfig({
         { hid: 'description', name: 'description', content: 'Salatak helps you generate and download ICS files for prayer times. Easily sync prayer schedules to your calendar and stay connected to your faith.' },
         { hid: 'og:title', property: 'og:title', content: 'Salatak - Your Prayer Times Companion' },
         { hid: 'og:description', property: 'og:description', content: 'Salatak helps you generate and download ICS files for prayer times. Easily sync prayer schedules to your calendar and stay connected to your faith.' },
-        { hid: 'og:image', property: 'og:image', content: 'https://salatak.netlify.app/images/salatak-og-image.jpg' },
-        { hid: 'og:url', property: 'og:url', content: 'https://salatak.netlify.app/' },
+        { hid: 'og:image', property: 'og:image', content: 'https://og.tailgraph.com/og?fontFamily=Roboto&title=Salatak&titleTailwind=font-bold%20text-6xl%20text-emerald-800&titleFontFamily=Roboto&text=Schedule%20your%20prayer%20times%20now%20!%20&textTailwind=text-gray-700%20text-2xl%20mt-4&logoTailwind=h-8&bgTailwind=bg-white&footer=tailgraph.com&footerTailwind=text-teal-600&t=1738073382939&refresh=1' },
+        { hid: 'og:url', property: 'og:url', content: 'https://salatak.vercel.app/' },
         { hid: 'twitter:title', name: 'twitter:title', content: 'Salatak - Your Prayer Times Companion' },
         { hid: 'twitter:description', name: 'twitter:description', content: 'Salatak helps you generate and download ICS files for prayer times. Easily sync prayer schedules to your calendar and stay connected to your faith.' },
-        { hid: 'twitter:image', name: 'twitter:image', content: 'https://salatak.netlify.app/images/salatak-twitter-image.jpg' },
+        { hid: 'twitter:image', name: 'twitter:image', content: 'https://og.tailgraph.com/og?fontFamily=Roboto&title=Salatak&titleTailwind=font-bold%20text-6xl%20text-emerald-800&titleFontFamily=Roboto&text=Schedule%20your%20prayer%20times%20now%20!%20&textTailwind=text-gray-700%20text-2xl%20mt-4&logoTailwind=h-8&bgTailwind=bg-white&footer=tailgraph.com&footerTailwind=text-teal-600&t=1738073382939&refresh=1' },
         { hid: 'twitter:card', name: 'twitter:card', content: 'summary_large_image' },
       ],
       link: [
         { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-        { rel: 'canonical', href: 'https://salatak.netlify.app/' },
+        { rel: 'canonical', href: 'https://salatak.vercel.app/' },
       ],
       htmlAttrs: {
         lang: 'en', // Default language
